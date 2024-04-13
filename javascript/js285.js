fce6e844555ba3c554fa0bcd28893b290f7a137a
Rochelle Williams <rochellew2343@gmail.com>
2024-04-13 15:40:07 -0400

    fixed javascript file connection and added javascript functions

--- javascript/js285.js
+++ javascript/js285.js
@@ -1,3 +1,5 @@
+// every page
+// mobile nav bar
 function myDropdownFunction() {
     var linksDiv = document.getElementById("mobile_nav_links");
     if (linksDiv.style.display === "block") {
@@ -5,4 +7,14 @@ function myDropdownFunction() {
     } else {
         linksDiv.style.display = "block";
     }
+}
+
+// home page
+// change intro image
+function changeIntroImageClick(){
+    document.getElementById("intro-image").src="images/hinata_jumping.gif"
+}
+
+function changeIntroImageBack(){
+    document.getElementById("intro-image").src="images/kuroko_cyclone_pass.gif"
 }
